d85e5d5d23ce984228195d0d992a0e4f71b1d2de
Ayah N <ayah.noor0213@gmail.com>
2021-01-17 13:48:10 -0500

    adding commit

--- src/day1/Main.java
+++ src/day1/Main.java
@@ -7,5 +7,9 @@ public class Main {
 
         System.out.println("Hello world");
         System.out.println("KAK DELA");
+        // some changes
+        System.out.println("gello");
+        System.out.println("=");
+        System.out.println("hello");
     }
 }
