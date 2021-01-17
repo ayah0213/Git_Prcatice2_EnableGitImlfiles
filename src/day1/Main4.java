66351fb490d474d01e697f94a1f44d316bc2f7e1
Ayah N <ayah.noor0213@gmail.com>
2021-01-17 13:55:54 -0500

    added 2nd nsg into 1 branch

--- src/day1/Main4.java
+++ src/day1/Main4.java
@@ -3,6 +3,7 @@ package day1;
 public class Main4 {
     public static void main(String[] args) {
         System.out.println("\"I just added new branch \" +\n" );
+        System.out.println("Ayajon");
     }
 
 }
