2dd23989137623b23c62d4911fc5c44da8dc6616
Ayah N <ayah.noor0213@gmail.com>
2021-01-16 19:53:02 -0500

    2nd commit

--- src/day1/Main.java
+++ src/day1/Main.java
@@ -5,13 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-         /*
-    What are the qualifications to join the Army?
-You must be a U.S. citizen or a resident alien.
-Be between the ages of 17-34. ...
-Have a high school diploma.
-Have no more than two dependents.
-     */
         System.out.println("Hello world");
+        System.out.println("KAK DELA");
     }
 }
---
+++ src/day1/Main2.java
@@ -0,0 +1,22 @@
+package day1;
+
+public class Main2 {
+     /*
+    What are the qualifications to join the Army?
+You must be a U.S. citizen or a resident alien.
+Be between the ages of 17-34. ...
+Have a high school diploma.
+Have no more than two dependents.
+     */
+     public static void main(String[] args) {
+         System.out.println("SELAMMMMMMM");
+     }
+     /*
+    What are the qualifications to join the Army?
+You must be a U.S. citizen or a resident alien.
+Be between the ages of 17-34. ...
+Have a high school diploma.
+Have no more than two dependents.
+     */
+
+}
